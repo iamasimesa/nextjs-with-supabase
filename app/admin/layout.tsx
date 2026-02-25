e14697452546269f96a519e0b77aa2189f002e83
iamasimesa <contactasimesa@gmail.com>
2026-02-26 03:59:13 +0400

    u

---
+++ app/admin/layout.tsx
@@ -0,0 +1,7 @@
+export default function LayoutAdminPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-xl font-semibold">Admin Orders</h1>
+    </main>
+  )
+}
---
+++ app/admin/products/page.tsx
@@ -0,0 +1,7 @@
+export default function ProductAdminPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-xl font-semibold">Admin Orders</h1>
+    </main>
+  )
+}
